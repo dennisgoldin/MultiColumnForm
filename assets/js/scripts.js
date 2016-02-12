ec86d9d194ea675e0fce5d4d0a9f3016beba79dd
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-12 00:01:26 -0500

    Add JavaScript and CSS.

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+var myForm = document.querySelector(".form");
+var myFormElements = myForm.elements;
+// console.log(myForm);
+// console.log(myFormElements);
+
+myForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(formData(myFormElements));
+});
+
+function formData(frm) {
+  var str = format("Type")
+          + format("Key")
+          + format("Value") + "\n"
+          + "-------------------------------------------\n";
+  for (var i = 0; i < frm.length; i++) {
+    str = str + format(frm[i].type)
+              + format(frm[i].name)
+              + format(frm[i].value) + "\n";
+  }
+  return str;
+}
+
+function format (str) {
+  var max = 15 - str.length;
+  for (var i = 0; i < max; i++) {
+    str = str + " ";
+  }
+  return str;
+}
